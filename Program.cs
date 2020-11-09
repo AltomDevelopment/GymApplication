59843921201e94660b92aeab257efbdfb508874a
AltomDevelopment <Zack.Altom@outlook.com>
2020-11-09 16:00:42 -0500

    simple logic setup

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ namespace GymApplication
     {
         static void Main(string[] args)
         {
-            var userStory = new UI();
+            UI userStory = new UI();
             userStory.GetUserInput("Enter your FirstName: ");
             /* Basic Functionality
              * What would you like this application to do?
              * Allow the user to enter the credentials to become a new gym member
              * Give the user a passcode that they will enter to access their workouts
              * Store a list of members and retrieve their workout list only after they have entered a passscode
-             * Break out categories for different workout types
+             * Break out categories for different workout types */
         }
     }
 }
--- UI.cs
+++ UI.cs
@@ -13,11 +13,17 @@ namespace GymApplication
         {
 
         }
-        public string userInput { get; set; }
-        public void GetUserInput(string prompt)
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Random Passcode = new Random(100);
+        public void GetUserInput()
         {
-            Console.WriteLine(prompt);
-            this.userInput = Console.ReadLine();
+            Console.WriteLine("Welcome to the gym");
+            Console.WriteLine("Please enter your First Name: ");
+            this.FirstName = Console.ReadLine();
+            Console.WriteLine("Please enter your Last Name: ");
+            this.LastName = Console.ReadLine();
         }
     }
 }
