f7377656b18c4d5498ea6dd7136d396964990301
AltomDevelopment <Zack.Altom@outlook.com>
2020-11-12 07:25:09 -0500

    added notes for functionality

--- GymMember.cs
+++ GymMember.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,5 +19,9 @@ namespace GymApplication
             this.lastName = lastName;
             this.passCode = passCode;
         }
+
+        //Come up with a workout layout strategy
+        //You should be able to layout the types of workouts, the weight, the reps, calories burned, timers
+        //You could add even more functionality then this
     }
 }
